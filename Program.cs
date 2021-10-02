000bf7662d193168cc1acba00ec3d5db3332c0b5
Ihor Moroz <ihor.moroz.17@gmail.com>
2021-10-02 23:17:08 +0300

    GetOwnerWithHighestDebt and GetFlatWithZeroUsage were added in QuarterReport class

--- Program.cs
+++ Program.cs
@@ -6,7 +6,12 @@ namespace ElectricityAccounting
     {
         static void Main()
         {
-            Console.WriteLine(new QuarterReport(@"C:\Users\ihorm\source\repos\ElectricityAccounting\QuarterReport.txt"));
+            var report = new QuarterReport(@"C:\Users\ihorm\source\repos\ElectricityAccounting\QuarterReport.txt");
+            Console.WriteLine(report);
+            Console.WriteLine($"{report.GetOwnerWithHighestDebt()} has highest debt");
+            if (report.GetFlatWithZeroUsage(out uint zeroUsage))
+                Console.WriteLine($"Flat #{zeroUsage} didn't use electricity at all");
+            else Console.WriteLine("All flats have been using electricity");
             Console.Read();
         }
     }
--- QuarterReport.cs
+++ QuarterReport.cs
@@ -10,6 +10,7 @@ namespace ElectricityAccounting
     {
         static string[,] _months;
         string[] _flatReports;
+        FlatRecord[] _records;
 
         static QuarterReport()
         {
@@ -24,7 +25,7 @@ namespace ElectricityAccounting
 
         public QuarterReport(uint quarter, params FlatRecord[] records)
         {
-            GetReport(quarter, records);
+            GetReport(quarter, _records = records);
         }
 
         public QuarterReport(string filePath)
@@ -32,13 +33,13 @@ namespace ElectricityAccounting
             var lines = System.IO.File.ReadAllLines(filePath);
             uint quarter = uint.Parse(lines[0].Split(' ')[1]);
 
-            var records = new FlatRecord[lines.Length - 1];
-            for (int i = 0; i < records.Length; ++i)
+            _records = new FlatRecord[lines.Length - 1];
+            for (int i = 0; i < _records.Length; ++i)
             {
-                FlatRecord.TryParse(lines[i + 1], out records[i]);
+                FlatRecord.TryParse(lines[i + 1], out _records[i]);
             }
 
-            GetReport(quarter, records);
+            GetReport(quarter, _records);
         }
 
         public QuarterReport() : this(0)
@@ -53,6 +54,33 @@ namespace ElectricityAccounting
             $"{_months[quarter - 1, 0]}: {rec[1] - rec[0]}kWh\n" +
             $"{_months[quarter - 1, 1]}: {rec[2] - rec[1]}kWh\n" +
             $"{_months[quarter - 1, 2]}: {rec[3] - rec[2]}kWh\n";
+
+        public string GetOwnerWithHighestDebt()
+        {
+            int max = 0;
+            for (int i = 1; i < _records.Length; ++i)
+                if (_records[max][3] - _records[max][0] <
+                    _records[i][3] - _records[i][0])
+                    max = i;
+
+            return _records[max].Owner;
+        }
+
+        //returns the first one found
+        public bool GetFlatWithZeroUsage(out uint flat)
+        {
+            for (int i = 0; i < _records.Length; ++i)
+            {
+                if (_records[i][3] - _records[i][0] == 0)
+                {
+                    flat = _records[i].ID;
+                    return true;
+                }
+            }
+
+            flat = 0;
+            return false;
+        }
 
         void GetReport(uint quarter, FlatRecord[] records)
         {
